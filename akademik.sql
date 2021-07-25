188a85a8cfe57b77a23464046eaaeaea2b3431c4
ImamRifaah <reallywhut11@gmail.com>
2021-07-25 12:16:40 +0800

    Project UAS

---
+++ akademik.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 25, 2021 at 06:03 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 7.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `akademik`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mahasiswa`
+--
+
+CREATE TABLE `mahasiswa` (
+  `Nim` int(10) NOT NULL,
+  `Nama_Mhs` varchar(50) NOT NULL,
+  `Jenis_Kelamin` varchar(50) NOT NULL,
+  `Program_Studi` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `mahasiswa`
+--
+
+INSERT INTO `mahasiswa` (`Nim`, `Nama_Mhs`, `Jenis_Kelamin`, `Program_Studi`) VALUES
+(2000018123, 'Taufikurrahman', 'Laki-Laki', 'Teknik Informatika'),
+(2000018150, 'Imam Rifa\'ah', 'Laki-Laki', 'Teknik Informatika'),
+(2000018185, 'Muhammad Salza Putra', 'Laki-Laki', 'Teknik Informatika'),
+(2000018190, 'Muhammad Farhan', 'Laki-Laki', 'Teknik Informatika'),
+(2000018199, 'Noval Abiyyu Arron', 'Laki-Laki', 'Teknik Informatika');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mahasiswa`
+--
+ALTER TABLE `mahasiswa`
+  ADD PRIMARY KEY (`Nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
